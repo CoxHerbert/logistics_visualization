91bfcae4fc5b06210a070d8e2733fdc9d61b8dfc
Cox Herbert <88811637+CoxHerbert@users.noreply.github.com>
2026-02-26 10:48:14 +0800

    fix(freight): disable tenant interceptor for lead mapper

--- ruoyi-vue-pro/yudao-module-freight/src/main/java/cn/iocoder/yudao/module/freight/dal/mysql/freight/FreightLeadMapper.java
+++ ruoyi-vue-pro/yudao-module-freight/src/main/java/cn/iocoder/yudao/module/freight/dal/mysql/freight/FreightLeadMapper.java
@@ -5,9 +5,11 @@ import cn.iocoder.yudao.framework.mybatis.core.mapper.BaseMapperX;
 import cn.iocoder.yudao.framework.mybatis.core.query.LambdaQueryWrapperX;
 import cn.iocoder.yudao.module.freight.controller.admin.freight.vo.lead.AdminFreightLeadPageReqVO;
 import cn.iocoder.yudao.module.freight.dal.dataobject.freight.FreightLeadDO;
+import com.baomidou.mybatisplus.annotation.InterceptorIgnore;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
+@InterceptorIgnore(tenantLine = "true")
 public interface FreightLeadMapper extends BaseMapperX<FreightLeadDO> {
 
     default PageResult<FreightLeadDO> selectPage(AdminFreightLeadPageReqVO reqVO) {
